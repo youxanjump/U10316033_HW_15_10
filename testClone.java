7f82fdf5293f7464dc64746294df6faac37f9bf1
youxanjump <maydayisgod@hotmail.com.tw>
2015-04-24 23:10:39 +0800

    註解

--- testClone.java
+++ testClone.java
@@ -1,13 +1,9 @@
 import java.util.ArrayList;
 
-class myStack implements Cloneable {
+class myStack implements Cloneable {//class myStack implements interface Cloneable
 
 	public ArrayList<Object> list = new ArrayList<>();
-	
-	public myStack(){
-		list = new ArrayList<Object>();
-	}
-	
+	//7~27,36~39 is useless method in this homework
 	public boolean isEmpty(){
 		return list.isEmpty();
 	}
@@ -29,11 +25,11 @@ class myStack implements Cloneable {
 	public void push(Object o){
 		list.add(o);
 	}
-	
+
 	@Override
 	public Object clone() throws CloneNotSupportedException{
-		myStack myStackClone = (myStack)super.clone();
-		myStackClone.list = (ArrayList<Object>)(list.clone());
+		myStack myStackClone = (myStack)super.clone();//shallow copy
+		myStackClone.list = (ArrayList<Object>)(list.clone());//deep copy
 		return myStackClone;
 	}
 	
@@ -46,10 +42,10 @@ class myStack implements Cloneable {
 public class testClone implements Cloneable{
 
 	public static void main(String args[]) throws CloneNotSupportedException{
-
+		//create 2 object with different reference but same value
 		myStack clone1 = new myStack();
 		myStack clone2 = (myStack)(clone1.clone());
-		
+		//show that have different reference
 		System.out.println("clone1 == clone2 ? " + (clone1 == clone2));
 		System.out.println("clone1.list == clone2.list? " + (clone1.list == clone2.list));
 
